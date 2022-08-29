4c7f9460f3468e52333fd3a55570ffcd63ea84cb
Sachin Mondal <sachinmondal161994@gmail.com>
2022-08-29 09:40:48 +0530

    Changes for real data

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 from sqlalchemy import create_engine
 import pandas as pd
 
@@ -5,31 +7,44 @@ mysql_usr = 'root'
 mysql_pwd = 'root'
 mysql_host = 'localhost'
 mysql_db = 'apms'
-table = 'raw_tbl'
+table = 'channel_data'
 
 db_connection_str = 'mysql+pymysql://' + mysql_usr + ':' + mysql_pwd + '@' + mysql_host + '/' + mysql_db
 db_connection = create_engine(db_connection_str)
 
+db_connection.execute("TRUNCATE TABLE active_tbl")
+db_connection.execute("TRUNCATE TABLE inactive_tbl")
+
+
 df = pd.read_sql('select * from apms.{}'.format(table), con=db_connection)
 
-print(df.values)
-print(df.columns)
+# print(df.values)
+# print(df.columns)
 
 counter = 0
 counter_two = 0
-df = df.sort_values(by='ts')
-state_prev = df['channel'][0]
+# df = df.sort_values(by='ts')
+state_prev = df['Ch_1652'][0]
 current_status = 'Off'
 
 
 def write_to_sql(data_f,tbl):
+
     data_fr = data_f.to_frame()
     data_fr = data_fr.swapaxes("index", "columns")
-    return data_fr.to_sql(name=tbl, con=db_connection, if_exists='append', index=False)
 
+
+    if tbl == 'active_tbl':
+        data_fr['ttf'] = random.randint(0, 1000)
+        return data_fr[['Date', 'Time', 'Ch_1652', 'ttf']].to_sql(name=tbl, con=db_connection, if_exists='append', index= False)
+
+    else:
+        return data_fr[['Date', 'Time', 'Ch_1652']].to_sql(name=tbl, con=db_connection, if_exists='append',
+                                                                index=False)
+    
 
 for ind in df.index:
-    if df['channel'][ind] == 'On' and state_prev == 'On':
+    if df['Ch_1652'][ind] == 'On' and state_prev == 'On':
         counter += 1
         if counter == 1:
             current_status = 'On'
@@ -37,16 +52,16 @@ for ind in df.index:
             write_to_sql(df.iloc[ind], 'active_tbl')
             print('always active table ',df.iloc[ind])
 
-    elif df['channel'][ind] == 'Off' and state_prev == 'Off' and current_status != 'On':
+    elif df['Ch_1652'][ind] == 'Off' and state_prev == 'Off' and current_status != 'On':
         write_to_sql(df.iloc[ind],'inactive_tbl')
         print('always inactive table ', df.iloc[ind])
 
-    elif df['channel'][ind] == 'On' and state_prev == 'Off':
+    elif df['Ch_1652'][ind] == 'On' and state_prev == 'Off':
         # Entry in On table
         write_to_sql(df.iloc[ind],'active_tbl')
         print('inactive to active ', df.iloc[ind])
 
-    elif df['channel'][ind] == 'Off' and current_status == 'On':
+    elif df['Ch_1652'][ind] == 'Off' and current_status == 'On':
         counter_two += 1
         if counter_two == 7:
             current_status = 'Off'
@@ -54,4 +69,4 @@ for ind in df.index:
             write_to_sql(df.iloc[ind],'inactive_tbl')
             print('active to inactive', df.iloc[ind])
 
-    state_prev = df['channel'][ind]
+    state_prev = df['Ch_1652'][ind]
